651a232e25bdb043e4b24b3da29479e5f734fc91
JeyGitHubpage <16leejames@gmail.com>
2023-07-02 23:04:43 -0400

    Create function when player chooses rock

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
 function getComputerChoice() {
     const choice = ['Rock', 'Paper', 'Scissors'] 
     let play = Math.floor(Math.random() * choice.length)
-    return console.log(play)
+    return console.log(choice.play)
 };
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    switch(playerChoice) {
+        case 'Rock':
+            if (computerSelection === 'Rock') {
+                console.log('Tie, both chose rock')
+            }
+
+            else if (computerselection === 'Paper') {
+                console.log('You lose, paper beats rock')
+            }
+
+            else {
+                console.log('You win, rock beats scissors')
+            }
+            #adsfdasf
+    }   
+}
